c1ee24b7c00c082506c4c8efd4b2b5b160f41d95
amit <amitsarker95@gmail.com>
2024-03-23 22:04:42 +0600

    Login page has been upgraded

--- src/Pages/LogIn/LogIn.jsx
+++ src/Pages/LogIn/LogIn.jsx
@@ -5,24 +5,25 @@ import passwordIcon from "./assets/padlock.png";
 
 const LogIn = () => {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen bg-[#F2F2F6]">
       {/* Left Image Section */}
-      <div className="flex flex-col justify-center items-center w-1/2 bg-[#F2F2F6]">
-        <div className="text w-full flex justify-center ">
-            <h1 className="font-bold">Wellcome to BestDeal</h1>
+      <div className="flex flex-col justify-center items-center w-1/2">
+        <div className="w-full flex justify-center">
+            <h1 className="text-[#2c296d] text-[54px] font-thin animate-slidein-left">Wellcome to BestDeal</h1>
         </div>
-        <img src={loginImage} alt="Image" />
+        
+        <img src={loginImage} alt="Image" className="w-[40%] animate-slidein-bottom" />
       </div>
 
       {/* Right Login Section */}
-      <div className="flex justify-center items-center w-1/2 bg-[#F2F2F6]">
-        <div className="p-8 max-w-md w-full">
-          <h2 className="text-3xl mb-4 text-center font-bold text-[#2C296D]">
+      <div className=" flex justify-center items-center w-1/2">
+        <div className="animate-slidein-top p-8 max-w-md w-full">
+          <h2 className="text-3xl mb-4 text-center font-bold text-[#2c296d]">
             Login
           </h2>
           <form className="space-y-4">
             <div className="flex flex-col">
-              <label htmlFor="email" className="text-sm mb-1">
+              <label htmlFor="email" className="text-sm mb-1 font-mono">
                 Email
               </label>
               <div className="relative">
@@ -34,14 +35,14 @@ const LogIn = () => {
                 <input
                   type="email"
                   id="email"
-                  className="w-full pl-[60px] py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-[#2d2a6e] hover:tracking-[1px] ease-linear hover:border-[#859A00]  transition duration-300"
-                  placeholder="Enter your email"
+                  className="w-full pl-[60px] py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-[#2d2a6e] tracking-[2px] ease-linear hover:border-[#859A00]  transition duration-300"
+                  placeholder="Enter your email..."
                   required="required"
                 />
               </div>
             </div>
             <div className="flex flex-col">
-              <label htmlFor="password" className="text-sm mb-1">
+              <label htmlFor="password" className="text-sm mb-1 font-mono">
                 Password
               </label>
               <div className="relative">
@@ -53,8 +54,8 @@ const LogIn = () => {
                 <input
                   type="password"
                   id="password"
-                  className=" w-full pl-[60px] py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-[#2d2a6e] hover:tracking-[1px] ease-linear hover:border-[#859A00]  transition duration-300"
-                  placeholder="Enter your password"
+                  className=" w-full pl-[60px] py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-[#2d2a6e] tracking-[2px] ease-linear hover:border-[#859A00]  transition duration-300"
+                  placeholder="Enter your password..."
                   required="required"
                 />
               </div>
