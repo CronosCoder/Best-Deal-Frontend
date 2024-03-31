da9aae9528745f7c5deea0fe76ce16420df33b8b
Foysal Ahmed <mfoysal314@gmail.com>
2024-04-01 03:05:18 +0600

    button and text modified in table

--- src/Components/Table/Cart/CartTableRow.jsx
+++ src/Components/Table/Cart/CartTableRow.jsx
@@ -18,16 +18,16 @@ const CartTableRow = () => {
             <td className='bg-white border'>
                 <img src={prodImg} alt="this is product image" className='w-36 h-36 mx-auto' />
             </td>
-            <td className='font-semibold text-xl border'>New and Fresh vegetables</td>
-            <td className='font-semibold text-xl border'>$ 130</td>
+            <td className='text-xl border'>New and Fresh vegetables</td>
+            <td className='text-xl border'>$ 130</td>
             <td className='border'>
                 <div className='grid grid-cols-3'>
-                    <div onClick={() => quantityHandler("-")} className='p-1 border bg-[#2D2A6E] text-white font-bold text-2xl text-center cursor-pointer rounded-l-md'>-</div>
-                    <div className='p-1 border font-bold text-3xl text-center'>{quantity}</div>
-                    <div onClick={() => quantityHandler("+")} className='p-1 border bg-[#2D2A6E] text-white font-bold text-2xl text-center cursor-pointer rounded-r-md'>+</div>
+                    <div onClick={() => quantityHandler("-")} className='p-1 border bg-[#96AE00] text-white font-bold text-2xl text-center cursor-pointer rounded-l-md'>-</div>
+                    <div className='p-1 border  text-2xl text-center'>{quantity}</div>
+                    <div onClick={() => quantityHandler("+")} className='p-1 border bg-[#96AE00] text-white font-bold text-2xl text-center cursor-pointer rounded-r-md'>+</div>
                 </div>
             </td>
-            <td className='font-bold text-2xl border text-[#2D2A6E]' >$ 130</td>
+            <td className='text-2xl border text-[#2D2A6E]' >$ 130</td>
             <td className='border'>
                 <button className='p-3  bg-red-100 duration-300 hover:bg-red-200 rounded-md'><RiDeleteBin3Fill size={20} color='#ff0000' /></button>
             </td>
--- src/Components/Table/WishList/WishListTableRow.jsx
+++ src/Components/Table/WishList/WishListTableRow.jsx
@@ -19,16 +19,16 @@ const WishListTableRow = () => {
             <td className='bg-white border'>
                 <img src={prodImg} alt="this is product image" className='w-36 h-36 mx-auto' />
             </td>
-            <td className='font-semibold text-xl border'>New and Fresh vegetables</td>
-            <td className='font-semibold text-xl border'>$ 130</td>
+            <td className='text-xl border'>New and Fresh vegetables</td>
+            <td className='text-xl border'>$ 130</td>
             <td className='border'>
                 <div className='grid grid-cols-3'>
-                    <div onClick={() => quantityHandler("-")} className='p-1 border bg-[#2D2A6E] text-white font-bold text-2xl text-center cursor-pointer rounded-l-md'>-</div>
-                    <div className='p-1 border font-bold text-3xl text-center'>{quantity}</div>
-                    <div onClick={() => quantityHandler("+")} className='p-1 border bg-[#2D2A6E] text-white font-bold text-2xl text-center cursor-pointer rounded-r-md'>+</div>
+                    <div onClick={() => quantityHandler("-")} className='p-1 border bg-[#96AE00] text-white font-bold text-2xl text-center cursor-pointer rounded-l-md'>-</div>
+                    <div className='p-1 border text-2xl text-center'>{quantity}</div>
+                    <div onClick={() => quantityHandler("+")} className='p-1 border bg-[#96AE00] text-white font-bold text-2xl text-center cursor-pointer rounded-r-md'>+</div>
                 </div>
             </td>
-            <td className='font-bold text-2xl border text-[#2D2A6E]' >$ 130</td>
+            <td className='text-2xl border text-[#2D2A6E]' >$ 130</td>
 
             <td className='border'>
                 <button className='p-3 bg-green-200  rounded-md'><FaCartPlus size={20} color='#2D2A6E' /></button>
